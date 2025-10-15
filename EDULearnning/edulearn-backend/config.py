189e939a476b87eb449844be1ceca21344e053ce
Adarsh Vishwakarma <vishwakarmaadarsh491@gmail.com>
2025-10-15 15:40:48 +0530

    Update config.py

--- EDULearnning/edulearn-backend/config.py
+++ EDULearnning/edulearn-backend/config.py
@@ -1,5 +1,6 @@
 import os
 
 class Config:
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://vishwakarmaadarsh9606_db_user:24QJ7RQFIOn2FypN@adarsh.q0b4unn.mongodb.net/edulearn?retryWrites=true&w=majority&appName=adarsh'
+    MONGO_URI = os.environ.get('MONGO_URI') or '//mongodb url'
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your_super_secret_key'
+
